36c7f64d43761b110149df97e29844bb85dd1e8c
june2174 <99578xunx@gmail.com>
2026-09-23 02:42:35 -0400

    Representational-basic form of alien

--- Assignmetns/Instructions/prototype1/template-p5-project/js/script.js
+++ Assignmetns/Instructions/prototype1/template-p5-project/js/script.js
@@ -1,24 +1,36 @@
 /**
- * Title of Project
- * Author Name
+ * Representational
+ * Juna Kim
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * I'll draw an alien
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Canvas size + I'll draw with noStroke
 */
 function setup() {
-
+    createCanvas(600, 600);
+    noStroke();
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * I'll draw alien with green body, pop colors, ..cute one
 */
 function draw() {
+    background(20, 10, 45);
 
+    //Shadow
+    fill(80, 220, 120);
+    ellipse(300, 530, 420, 45);
+
+    //Body
+    fill(120, 255, 120);
+    ellipse(300, 390, 210, 250);
+
+    //Head
+    fill(140, 255, 150);
+    ellipse(300, 220, 280, 230);
 }
