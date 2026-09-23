f7193286f3e88cdb611a3601f686b7c4036c3756
june2174 <99578xunx@gmail.com>
2026-09-23 03:12:25 -0400

    abstract-alien progress

--- Assignmetns/Instructions/prototype2/template-p5-project/js/script.js
+++ Assignmetns/Instructions/prototype2/template-p5-project/js/script.js
@@ -1,24 +1,47 @@
 /**
- * Title of Project
- * Author Name
+ * Abstract-Alien
+ * Juna Kim
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Geometrical version of alien with pop colors pink!
+ * 
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Canvas size + I'll draw with noStroke
 */
 function setup() {
-
+    createCanvas(600, 600);
+    noStroke();
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * I'll draw a geometrical alien with pop colors
 */
 function draw() {
+    background(255, 80, 160);
 
+    //Head
+    fill(80, 230, 255);
+    ellipse(300, 300, 380, 330);
+
+    //Outer circles
+    fill(180, 70, 240);
+    ellipse(300, 300, 290, 250);
+    fill(255, 210, 50);
+    ellipse(300, 300, 210, 180);
+
+    //Eyes
+    fill(30, 20, 80);
+    ellipse(235, 280, 80, 110);
+    ellipse(365, 280, 80, 110);
+    fill(255, 80, 180);
+    ellipse(235, 280, 35, 65);
+    ellipse(365, 280, 35, 65);
+
+    //Mouth
+    fill(80, 230, 255);
+    ellipse(300, 350, 50, 50);
 }
