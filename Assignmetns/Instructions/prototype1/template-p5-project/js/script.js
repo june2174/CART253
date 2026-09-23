2f3383f0354b89a527d05559be66f7a754715b53
june2174 <99578xunx@gmail.com>
2026-09-23 02:49:45 -0400

    representational-antenna

--- Assignmetns/Instructions/prototype1/template-p5-project/js/script.js
+++ Assignmetns/Instructions/prototype1/template-p5-project/js/script.js
@@ -26,6 +26,27 @@ function draw() {
     fill(80, 220, 120);
     ellipse(300, 530, 420, 45);
 
+    //Antenna
+    stroke(180, 255, 80);
+    strokeWeight(8);
+    line(250, 180, 210, 110);
+    line(350, 180, 390, 110);
+    noStroke();
+
+    //Antenna balls
+    fill(255, 80, 180);
+    ellipse(210, 105, 30, 30);
+    fill(80, 220, 255);
+    ellipse(390, 105, 30, 30);
+
+    // Arms
+    noFill();
+    stroke(100, 230, 100);
+    strokeWeight(20);
+    bezier(210, 330, 150, 350, 150, 430, 185, 475);
+    bezier(390, 330, 450, 350, 450, 430, 415, 475);
+    noStroke();
+
     //Body
     fill(120, 255, 120);
     ellipse(300, 390, 210, 250);
