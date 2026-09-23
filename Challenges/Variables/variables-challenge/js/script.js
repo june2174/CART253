813603afeff2a21e29a1ab9c1a1f5ef74763c0a0
june2174 <99578xunx@gmail.com>
2026-09-23 18:48:28 -0400

    gradually changed mrfurious face& sky color

--- Challenges/Variables/variables-challenge/js/script.js
+++ Challenges/Variables/variables-challenge/js/script.js
@@ -21,6 +21,14 @@ let mrFurious = {
     }
 };
 
+let sky = {
+    fill: {
+        r: 160,
+        g: 180,
+        b: 200
+    }
+};
+
 /**
  * Create the canvas
  */
@@ -32,7 +40,18 @@ function setup() {
  * Draw (and update) Mr. Furious
  */
 function draw() {
-    background(160, 180, 200);
+    background(sky.fill.r, sky.fill.g, sky.fill.b);
+    sky.fill.r = sky.fill.r * 0.9999;
+    sky.fill.g = sky.fill.g * 0.9999;
+    sky.fill.b = sky.fill.b * 0.9999;
+    sky.fill.r = constrain(sky.fill.r, 20, 200);
+    sky.fill.g = constrain(sky.fill.g, 30, 200);
+    sky.fill.b = constrain(sky.fill.b, 50, 200);
+
+    mrFurious.fill.g = mrFurious.fill.g * 0.999;
+    mrFurious.fill.b = mrFurious.fill.b * 0.999;
+    mrFurious.fill.g = constrain(mrFurious.fill.g, 100, 225);
+    mrFurious.fill.b = constrain(mrFurious.fill.b, 100, 225);
 
     // Draw Mr. Furious as a coloured circle
     push();
