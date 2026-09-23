f0b439b941cc14d008f090c79e976230ff74e57f
june2174 <99578xunx@gmail.com>
2026-09-23 04:26:55 -0400

    weird-writing function

--- Assignmetns/Instructions/prototype3/template-p5-project/js/script.js
+++ Assignmetns/Instructions/prototype3/template-p5-project/js/script.js
@@ -1,24 +1,64 @@
 /**
- * Title of Project
- * Author Name
+ * Weird-Alien
+ * Juna Kim
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * I'll make a mosaic pattern of representational alien and abstract alien
+ * 
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Canvas size + I'll draw with noStroke
 */
 function setup() {
-
+    createCanvas(600, 600);
+    noStroke();
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * background and mosaic pattern loop
 */
 function draw() {
+    background(20, 10, 45);
 
+    let index = 0;
+
+    for (let x = 15; x < 600; x += 15) {
+        for (let y = 15; y < 600; y += 15) {
+
+            //representational and abstract
+            if (index % 2 == 0) {
+                drawAlienTile(x, y, "representational");
+            } else {
+                drawAlienTile(x, y, "abstract");
+            }
+
+            index++;
+        }
+    }
+}
+
+/**
+ * Draw one tiny alien head
+ */
+function drawAlienTile(x, y, type) {
+
+    if (type == "representational") {
+
+        // Green alien head
+        fill(140, 255, 150);
+        ellipse(x, y, 12, 10);
+
+        // Eyes
+        fill(30, 20, 70);
+        ellipse(x - 3, y, 2, 4);
+        ellipse(x + 3, y, 2, 4);
+
+        // Nose
+        fill(255, 210, 60);
+        ellipse(x, y + 2, 2, 2);
+
+    }
 }
